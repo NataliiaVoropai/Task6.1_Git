e466131310fd8685207bb45a676c1473f3aed48d
Natalia Voropai <natalia.voropai@uvoteam.com>
2024-02-22 20:19:44 +0100

    Added division

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ def calculate(args) -> float:
         return args.num1 + args.num2
     elif args.operation == 'substract':
         return args.num1 - args.num2
+    elif args.operation == 'div':
+        return args.num1 / args.num2
     else:
         return 'Unsupported operation'
 
 
 def main():
-    parser = argparse.AtgumentParser(descritpion="Calculator: addition and substraction")
-    parser.add_argument("operation", choices=['add', 'subsctract'], help='operation(add or subsctract)')
+    parser = argparse.AtgumentParser(description="Calculator: addition,"
+                                     "substraction, division, and"
+                                     "multiplication")
+    parser.add_argument("operation", choices=['add', 'subsctract', 'div'],
+                        help='operation(add, substract, divide)')
     parser.add_argument("num1", type=float, help="The first number")
     parser.add_argument("num2", type=float, help="The second number")
     args = parser.parse_args()
